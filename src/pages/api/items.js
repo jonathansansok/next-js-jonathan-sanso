8f155c6740dbfdff2beccdc61d343f328fc97824
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-04-23 13:51:25 -0300

    static-generation-json-bien

--- src/pages/api/items.js
+++ src/pages/api/items.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   res.status(200).json([
     {
       id: 0,
-      title: "Figurita octocat",
+      title: "Figurita octocat chica",
       price: 100,
       image: "http://localhost:3000/img/octocat.webp",
       description: "Lorem ipsum dolor sit amet, consectetur adip"
--- src/pages/store/index.js
+++ src/pages/store/index.js
@@ -1,9 +1,23 @@
 import Link from "next/link";
 import Layout from "../../../components/layout";
-export default function Index() {
+import { getItems } from "../../../services/itemService";
+export default function Index({items}) {
   return (
     <Layout>
       <h1>Store</h1>
+      {
+        items && items.map(item => <div key={item.div}>{item.title}</div>)
+      }
     </Layout>
   );
+
+
+}
+export async function getStaticProps(){
+ const res = await getItems() ;
+ return {
+    props: {
+        items: res,
+    }
+ }
 }
