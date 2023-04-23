d6c55ec2b68f1c37a94fbaa398add93d349afadd
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-04-23 12:06:07 -0300

    api-json-productos

---
+++ src/pages/api/items.js
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      id: 0,
+      title: "Figurita octocat",
+      price: 100,
+      image: "http://localhost:3000/img/octocat.webp",
+      description: "Lorem ipsum dolor sit amet, consectetur adip"
+    },
+  ])
+}
--- src/pages/store/index.js
+++ src/pages/store/index.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import Layout from "../../../components/layout";
-export default function Index(){
-    return(
-        <Layout>
-            <h1>Store</h1>
-        </Layout>
-    )
+export default function Index() {
+  return (
+    <Layout>
+      <h1>Store</h1>
+    </Layout>
+  );
 }
