4751edc1c0262df5d8d7f378c6f420b2ccc1abd0
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-04-25 18:16:43 -0300

    inicio-ruteo-dinamic

--- src/pages/store/index.js
+++ src/pages/store/index.js
@@ -1,26 +1,27 @@
 import Link from "next/link";
 import Layout from "../../../components/layout";
-import Image from 'next/image'
+import Image from "next/image";
 import { getItems } from "../../../services/itemService";
 import Product from "../../../components/product";
-export default function Index({items}) {
+import styleItems from  "../../styles/product.module.css"
+export default function Index({ items }) {
   return (
     <Layout>
       <h1>Store</h1>
-      {
-        items && items.map(item => (
-          <Product key={item.id}item={item} showAs="Default"/>)
-      )}
+      <div className={styleItems.items}>
+        {items &&
+          items.map((item) => (
+            <Product key={item.id} item={item} showAs="Default" />
+          ))}
+      </div>
     </Layout>
   );
-
-
 }
-export async function getStaticProps(){
- const res = await getItems() ;
- return {
+export async function getStaticProps() {
+  const res = await getItems();
+  return {
     props: {
-        items: res,
-    }
- }
+      items: res,
+    },
+  };
 }
